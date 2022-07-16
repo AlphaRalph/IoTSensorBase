e65646b3735ea26845079cfcf723fcd6b3ce6de0
andrejpervan <61869502+andrejpervan@users.noreply.github.com>
2022-07-16 19:51:59 +0200

    Update and rename TempHumiditySensor.py to dht_example.py

--- Python/dht_example.py
+++ Python/dht_example.py
@@ -1,7 +1,6 @@
 import time
 import board
 import adafruit_dht
-import mh_z19
  
 # Initial the dht device, with data pin connected to:
 # dhtDevice = adafruit_dht.DHT22(board.D4)
@@ -15,22 +14,20 @@ while True:
     try:
         # Print the values to the serial port
         temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
         print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
+            "Temp: {:.1f} C    Humidity: {}% ".format(
+                temperature_c, humidity
             )
         )
-        print(mh_z19.readall())
  
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
-        time.sleep(2.0)
+        time.sleep(2.0) # Maximum reading time DHT22
         continue
     except Exception as error:
         dhtDevice.exit()
         raise error
  
-    time.sleep(2.0) # Maximum time for DHT22
+    time.sleep(2.0) # Maximum reading time DHT22
